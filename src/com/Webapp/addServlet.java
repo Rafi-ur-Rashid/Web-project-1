9f103b4e8dec5b9e2438c2d7ded2bf473d8a8aea
unknown <rafiurrashid150@gmail.com>
2020-09-08 21:39:46 +0600

    Squareservlet context and config added

--- src/com/Webapp/addServlet.java
+++ src/com/Webapp/addServlet.java
@@ -29,5 +29,6 @@ public class addServlet extends HttpServlet{
 		res.addCookie(cookie);
 		
 		res.sendRedirect("sq");
+		
 	}
 }
--- src/com/Webapp/squareServlet.java
+++ src/com/Webapp/squareServlet.java
@@ -3,6 +3,9 @@ package com.Webapp;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,5 +28,13 @@ public class squareServlet extends HttpServlet{
 		}
 		
 		res.getWriter().println("the sq is: "+ k*k);
+		
+		ServletContext cts=getServletContext();
+		String s=cts.getInitParameter("Name");
+		res.getWriter().println(s);
+		
+		ServletConfig cg=getServletConfig();
+		String s1=cg.getInitParameter("Name");
+		res.getWriter().println(s1);
 	}
 }
